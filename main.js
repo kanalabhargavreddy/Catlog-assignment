d2661903ec8a09724653389ceb9a71ba8bfc4e22
BHARGAV REDDY <108825247+kanalabhargavreddy@users.noreply.github.com>
2024-12-30 12:29:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
+
 function decodeBaseValue(base, value) {
     return parseInt(value, base);
 }
+
 function lagrangeInterpolation(points, k) {
     let constantTerm = 0;
 
@@ -22,13 +24,12 @@ function lagrangeInterpolation(points, k) {
 
     return constantTerm;
 }
+
 function findSecretConstant(jsonData) {
     let n = jsonData.keys.n;
     let k = jsonData.keys.k;
 
     let points = [];
-
-    // Iterate over each key in the object
     Object.keys(jsonData).forEach(key => {
         if (key !== 'keys') {
             let base = parseInt(jsonData[key].base);
@@ -44,81 +45,14 @@ function findSecretConstant(jsonData) {
     return lagrangeInterpolation(points, k);
 }
 
-function processTestCases(testCases) {
-    testCases.forEach((testCase, index) => {
-        let result = findSecretConstant(testCase);
-        console.log(`The constant term (c) for test case ${index + 1} is: ${result}`);
+function processTestCasesFromFile(fileNames) {
+    fileNames.forEach((fileName, index) => {
+        const jsonData = JSON.parse(fs.readFileSync(fileName, 'utf8'));
+        let result = findSecretConstant(jsonData);
+        console.log(`Secret key for test case ${index + 1} from file ${fileName} is: ${result}`);
     });
 }
-const testCase1 = {
-    "keys": {
-        "n": 4,
-        "k": 3
-    },
-    "1": {
-        "base": "10",
-        "value": "4"
-    },
-    "2": {
-        "base": "2",
-        "value": "111"
-    },
-    "3": {
-        "base": "10",
-        "value": "12"
-    },
-    "6": {
-        "base": "4",
-        "value": "213"
-    }
-}
 
-
-const testCase2 = {
-"keys": {
-    "n": 10,
-    "k": 7
-  },
-  "1": {
-    "base": "6",
-    "value": "13444211440455345511"
-  },
-  "2": {
-    "base": "15",
-    "value": "aed7015a346d63"
-  },
-  "3": {
-    "base": "15",
-    "value": "6aeeb69631c227c"
-  },
-  "4": {
-    "base": "16",
-    "value": "e1b5e05623d881f"
-  },
-  "5": {
-    "base": "8",
-    "value": "316034514573652620673"
-  },
-  "6": {
-    "base": "3",
-    "value": "2122212201122002221120200210011020220200"
-  },
-  "7": {
-    "base": "3",
-    "value": "20120221122211000100210021102001201112121"
-  },
-  "8": {
-    "base": "6",
-    "value": "20220554335330240002224253"
-  },
-  "9": {
-    "base": "12",
-    "value": "45153788322a1255483"
-  },
-  "10": {
-    "base": "7",
-    "value": "1101613130313526312514143"
-  }
-}
-const testCases = [testCase1, testCase2];
-processTestCases(testCases);
+// Array of file names containing the test cases
+const testCaseFiles = ['./testcase1.json', './testcase2.json'];
+processTestCasesFromFile(testCaseFiles);
