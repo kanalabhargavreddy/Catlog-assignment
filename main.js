0cbd85136db170aa49d4e32185df26de993dce79
BHARGAV REDDY <108825247+kanalabhargavreddy@users.noreply.github.com>
2024-12-30 12:12:50 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,124 @@
+const fs = require('fs');
+function decodeBaseValue(base, value) {
+    return parseInt(value, base);
+}
+function lagrangeInterpolation(points, k) {
+    let constantTerm = 0;
+
+    for (let i = 0; i < k; i++) {
+        let xi = points[i].x;
+        let yi = points[i].y;
+        let li = 1;
+
+        for (let j = 0; j < k; j++) {
+            if (i !== j) {
+                let xj = points[j].x;
+                li *= -xj / (xi - xj);
+            }
+        }
+
+        constantTerm += yi * li;
+    }
+
+    return constantTerm;
+}
+function findSecretConstant(jsonData) {
+    let n = jsonData.keys.n;
+    let k = jsonData.keys.k;
+
+    let points = [];
+
+    // Iterate over each key in the object
+    Object.keys(jsonData).forEach(key => {
+        if (key !== 'keys') {
+            let base = parseInt(jsonData[key].base);
+            let value = jsonData[key].value;
+            let x = parseInt(key);  // 'key' is the x-value
+            let y = decodeBaseValue(base, value);  // Decode the y-value
+            points.push({ x, y });
+        }
+    });
+
+    points.sort((a, b) => a.x - b.x);  // Sort points by x-value
+
+    return lagrangeInterpolation(points, k);
+}
+
+function processTestCases(testCases) {
+    testCases.forEach((testCase, index) => {
+        let result = findSecretConstant(testCase);
+        console.log(`The constant term (c) for test case ${index + 1} is: ${result}`);
+    });
+}
+const testCase1 = {
+    "keys": {
+        "n": 4,
+        "k": 3
+    },
+    "1": {
+        "base": "10",
+        "value": "4"
+    },
+    "2": {
+        "base": "2",
+        "value": "111"
+    },
+    "3": {
+        "base": "10",
+        "value": "12"
+    },
+    "6": {
+        "base": "4",
+        "value": "213"
+    }
+}
+
+
+const testCase2 = {
+"keys": {
+    "n": 10,
+    "k": 7
+  },
+  "1": {
+    "base": "6",
+    "value": "13444211440455345511"
+  },
+  "2": {
+    "base": "15",
+    "value": "aed7015a346d63"
+  },
+  "3": {
+    "base": "15",
+    "value": "6aeeb69631c227c"
+  },
+  "4": {
+    "base": "16",
+    "value": "e1b5e05623d881f"
+  },
+  "5": {
+    "base": "8",
+    "value": "316034514573652620673"
+  },
+  "6": {
+    "base": "3",
+    "value": "2122212201122002221120200210011020220200"
+  },
+  "7": {
+    "base": "3",
+    "value": "20120221122211000100210021102001201112121"
+  },
+  "8": {
+    "base": "6",
+    "value": "20220554335330240002224253"
+  },
+  "9": {
+    "base": "12",
+    "value": "45153788322a1255483"
+  },
+  "10": {
+    "base": "7",
+    "value": "1101613130313526312514143"
+  }
+}
+const testCases = [testCase1, testCase2];
+processTestCases(testCases);
